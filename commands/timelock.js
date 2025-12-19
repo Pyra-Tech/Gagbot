48f1359e5a4c0b9b18a2b07d07c09ed271b7c2e5
Totox00 <bladewielder.roboman@gmail.com>
2025-12-19 02:06:27 +0100

    handle trying to timelock already timelocked belt

--- commands/timelock.js
+++ commands/timelock.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 const { timelockChastityModal } = require("./../functions/interactivefunctions.js");
-const { getChastityKeyholder, getChastity } = require("../functions/vibefunctions.js");
+const { getChastity } = require("../functions/vibefunctions.js");
 const { their } = require("../functions/pronounfunctions.js");
+const { getHeavy } = require("../functions/heavyfunctions.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -21,7 +22,9 @@ module.exports = {
 
     switch (actiontotake) {
       case "chastity":
-        if (!getChastity(wearer.id)) {
+        const chastity = getChastity(wearer.id);
+
+        if (!chastity) {
           interaction.reply({
             content: `${wearer} is not wearing a belt`,
             flags: MessageFlags.Ephemeral,
@@ -29,7 +32,15 @@ module.exports = {
           return;
         }
 
-        if (getChastityKeyholder(wearer.id) != interaction.user.id) {
+        if (chastity.unlockTime) {
+          interaction.reply({
+            content: `${wearer}'s belt is already timelocked`,
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
+
+        if (chastity.keyholder != interaction.user.id) {
           interaction.reply({
             content: `You do not have the key to ${wearer}'s belt`,
             flags: MessageFlags.Ephemeral,
@@ -42,10 +53,12 @@ module.exports = {
             interaction.reply(
               `${interaction.user} pulls against ${their(wearer.id)} ${getHeavy(interaction.user.id).type} trying to apply a timelock to ${their(wearer.id)} chastity belt, but is completely stuck!`
             );
+            return;
           } else {
             interaction.reply(
               `${interaction.user} pulls against ${their(wearer.id)} ${getHeavy(interaction.user.id).type} trying to apply a timelock to ${wearer}'s chastity belt, but is completely stuck!`
             );
+            return;
           }
         }
 
