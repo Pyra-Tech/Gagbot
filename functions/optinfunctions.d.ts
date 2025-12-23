63e523ac62b146e0fde2aa42ba4696fae6fcd804
Totox00 <bladewielder.roboman@gmail.com>
2025-12-23 17:19:29 +0100

    more typedef fixes

--- functions/optinfunctions.d.ts
+++ functions/optinfunctions.d.ts
@@ -1,16 +1,16 @@
 export type OptinTypes = "KeyGiving" | "EnableVibes" | "DynamicArousal" | "KeyFumbling" | "OthersKeyFumbling" | "FumbleOthersKeys" | "BlessedLuck" | "KeyDiscarding" | "AnyFinders";
-export type OptinMap = { [key: OptinTypes]: [number, string, string] };
+export type OptinMap = { [Optin in OptinTypes]: [number, string, string] };
 export type Optins = {
   [Optin in OptinTypes as `set${Optin}`]: (user: number) => undefined;
 } & {
   [Optin in OptinTypes as `unset${Optin}`]: (user: number) => undefined;
 } & {
-  [Optin in OptinTypes as `get${Optin}`]: (user: number) => bool;
+  [Optin in OptinTypes as `get${Optin}`]: (user: number) => boolean;
 };
 
 export const optinMap: OptinMap;
-export function setOptin(user: number, offset: number);
-export function unsetOptin(user: number, offset: number);
-export function getOptin(user: number, offset: number): bool;
-export function optinIsLocked(user: number, offset: number): string?;
+export function setOptin(user: number, offset: number): void;
+export function unsetOptin(user: number, offset: number): void;
+export function getOptin(user: number, offset: number): boolean;
+export function optinIsLocked(user: number, offset: number): string | null;
 export const optins: Optins;
