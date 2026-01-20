5fab40a170863cf025d3fef4a32ae11a8d501c33
dollliandra <dollliandra@users.noreply.github.com>
2026-01-20 20:10:04 +0000

    Style: Format with Prettier

--- functions/dollminatrix_ast.js
+++ functions/dollminatrix_ast.js
@@ -15,67 +15,69 @@
 //                |-  Tags -| |>///<| |Match code block | |------------ ANSI Color Username Block --------| |-ANSI Colors -| |-- Match italic text (ignore escaped asterisks)  -------| |--------  Match underscore italic text --------| |----------------------  Match website URLs     ---------------------------------------------------| |---- Emojis ----| |--- Unicode Emoji -----------------------------------------------|
 const oldregex = /(<@[0-9]+>)|(>\/+<)|(```((ansi|js)\n)?)|(\u001b\[[0-9];[0-9][0-9]m([^\u0000-\u0020]+: ?))|(\u001b\[.+?m) ?|((\-#\s+)?((?<![\*\\])\*{1})(\*{2})?(\\\*|[^\*]|\*{2})+\*)|((\-#\s+)?((?<!\_)\_{1})(\_{2})?([^\_]|\_{2})+\_)|(<?https?\:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)>?)|(<a?:[^:]+:[^>]+>)|(\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])|\n/g;
 
-const REGEX_OOC = /(?<OOC>((\-#\s+)?((?<![\*\\])\*{1})(\*{2})?(\\\*|[^\*]|\*{2})+\*)|((\-#\s+)?((?<!\_)\_{1})(\_{2})?([^\_]|\_{2})+\_))/g
+const REGEX_OOC = /(?<OOC>((\-#\s+)?((?<![\*\\])\*{1})(\*{2})?(\\\*|[^\*]|\*{2})+\*)|((\-#\s+)?((?<!\_)\_{1})(\_{2})?([^\_]|\_{2})+\_))/g;
 
 const REGEX_SENTENCE = /(?<tag><@[0-9]+>)|(?<textEmote>(>\/+<))|(?<codeBlock>```((ansi|js)\n)?)|(?<ANSIUsername>\u001b\[[0-9];[0-9][0-9]m([^\u0000-\u0020]+: ?))|(?<ANSIColor>\u001b\[.+?m) ?|(?<websiteURL><?https?\:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)>?)|(?<emoji><a?:[^:]+:[^>]+>)|(?<uncodeEmoji>\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])|\n/g;
 
 const splitMessageV2Push = (arr, text, type) => {
-    let newObject = {"type": type}
+	let newObject = { type: type };
 
-    if(["IC","OOC"].includes(type)){
-        newObject["data"] = text
-    }else{
-        newObject["text"] = text;
-    }
-    
-    arr.push(newObject)
-}
+	if (["IC", "OOC"].includes(type)) {
+		newObject["data"] = text;
+	} else {
+		newObject["text"] = text;
+	}
 
-const splitMessageV2 = (text, inRegex=REGEX_OOC, base=true) => {
-    // RegExp have writable properties - get a fresh copy just in case of synchronization issues.
-    let regex = new RegExp(inRegex.source,"g")
+	arr.push(newObject);
+};
 
-    let output = []
+const splitMessageV2 = (text, inRegex = REGEX_OOC, base = true) => {
+	// RegExp have writable properties - get a fresh copy just in case of synchronization issues.
+	let regex = new RegExp(inRegex.source, "g");
 
-    // Split IC/OOC
-    let curr, chunk
-    let startIndex = 0;
-    while((curr = regex.exec(text)) !== null){
+	let output = [];
 
-        // If anything is before the match, snag it.
-        if(startIndex != curr.index){
-            chunk = text.substring(startIndex,curr.index)
-            if(base){
-                splitMessageV2Push(output, splitMessageV2(chunk,REGEX_SENTENCE,false), "IC")
-            }else{
-                splitMessageV2Push(output,chunk, "rawText")
-            }
-            console.log(`Chunk: ${chunk}`)
-        }
+	// Split IC/OOC
+	let curr, chunk;
+	let startIndex = 0;
+	while ((curr = regex.exec(text)) !== null) {
+		// If anything is before the match, snag it.
+		if (startIndex != curr.index) {
+			chunk = text.substring(startIndex, curr.index);
+			if (base) {
+				splitMessageV2Push(output, splitMessageV2(chunk, REGEX_SENTENCE, false), "IC");
+			} else {
+				splitMessageV2Push(output, chunk, "rawText");
+			}
+			console.log(`Chunk: ${chunk}`);
+		}
 
-        // Get the match itself.
-        splitMessageV2Push(output, curr[0], Object.keys(curr.groups).find((e) => {return !!curr.groups[e]}))
-        console.log(`Match: ${curr[0]}`)
-        startIndex = regex.lastIndex
-    }
+		// Get the match itself.
+		splitMessageV2Push(
+			output,
+			curr[0],
+			Object.keys(curr.groups).find((e) => {
+				return !!curr.groups[e];
+			}),
+		);
+		console.log(`Match: ${curr[0]}`);
+		startIndex = regex.lastIndex;
+	}
 
-    // Get the rest of the text.
-    if(startIndex < text.length){
-        chunk = text.substring(startIndex)
-        splitMessageV2Push(output,chunk, "rawText")
-        console.log(`Chunk: ${chunk}`)
-    }
+	// Get the rest of the text.
+	if (startIndex < text.length) {
+		chunk = text.substring(startIndex);
+		splitMessageV2Push(output, chunk, "rawText");
+		console.log(`Chunk: ${chunk}`);
+	}
 
-    return output
-}
-
-
+	return output;
+};
 
 // Unit Testing
 
-let strA = "Test meowssage. >///< *Italics meowssage.* More text. *Meowre text!*uwu"
-let strA_result = splitMessageV2(strA)
+let strA = "Test meowssage. >///< *Italics meowssage.* More text. *Meowre text!*uwu";
+let strA_result = splitMessageV2(strA);
 
-console.log(strA)
-console.log(strA_result)
-
+console.log(strA);
+console.log(strA_result);
