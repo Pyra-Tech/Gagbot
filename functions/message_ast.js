07158e407d1789447dd7bc8122452b09454c2593
Enraa <enraaaery@gmail.com>
2026-01-22 22:34:25 -0800

    Revert "aaaaaaaa"
    This reverts commit 561d969f795e71a95a9ec545cd80d8809319687f.

--- functions/message_ast.js
+++ functions/message_ast.js
@@ -103,7 +103,7 @@ class MessageAST {
         // Dollminatrix can cry and rip out my patchwork after applying percussive maintenace 
         // to the corset function probably. 
         let outofbreath = ["-# *Panting heavily*", "-# *Completely out of breath*", "-# *Desperately gasping for air*", "-# *About to pass out*"]
-        output = output.replace("[object Object]", outofbreath[Math.random(Math.floor(outofbreath.length * Math.random()))])
+        output = output.replace("[Object object]", outofbreath[Math.random(Math.floor(outofbreath.length * Math.random()))])
 		return output;
 	}
 }
