5f6fd8ef652f1a041c00ad5fc5be7c299f70d225
Enraa <enraaaery@gmail.com>
2026-01-26 18:47:17 -0800

    PLEASE SOMEONE COME FOR US

---
+++ chastity/belt/belt_hardlight.js
@@ -0,0 +1,5 @@
+// Standard belt
+exports.denialCoefficient = (data) => { return 10 }
+
+// Name
+exports.name = "Hardlight Belt"
---
+++ chastity/belt/belt_wolf.js
@@ -0,0 +1,5 @@
+// Standard belt
+exports.denialCoefficient = (data) => { return 7.5 }
+
+// Name
+exports.name = "Wolf Panties"
