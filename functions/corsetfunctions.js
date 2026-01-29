6ac2906b0b4317753c7551b705f090b9f1a3ee0a
Totox00 <bladewielder.roboman@gmail.com>
2026-01-29 07:54:15 +0100

    actually make the different types do things

--- functions/corsetfunctions.js
+++ functions/corsetfunctions.js
@@ -4,17 +4,82 @@ const nlp = require("compromise");
 const nlpSpeech = require("compromise-speech");
 nlp.extend(nlpSpeech);
 
-const TRAITS = ["name"];
+const TRAITS = ["name", "maxBreath", "minBreath", "breathRecovery", "gaspCoefficient", "gaspLimit", "silenceLimit", "minWords", "afterUsingBreath"];
 
-const DEFAULT_CORSET = { name: "Leather Corset", type: "corset_leather" };
+const DEFAULT_CORSET = {
+	name: "Leather Corset",
+	type: "corset_leather",
+	maxBreath: [2000, 56, 48, 40, 34, 28, 24, 20, 16, 13, 10, 7, 4, 3, 2, 2],
+	minBreath: [0, -120, -116, -112, -108, -104, -96, -88, -80, -72, -60, -60, -48, -40, -30, -20],
+	breathRecovery: [2000, 4.6, 3.8, 3.2, 2.6, 2, 1.6, 1.28, 1, 0.8, 0.6, 0.4, 0.2, 0.1, 0.04, 0.008],
+	gaspCoefficient: 1,
+	gaspLimit: [1000, 28, 24, 20, 17, 14, 12, 10, 8, 6.5, 5, 3.5, 2, 1.5, 1, 1],
+	silenceLimit: [-2000, -56, -48, -40, -34, -28, -24, -20, -16, -13, -10, -7, -4, -3, -2, -2],
+	minWords: [10, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0, 0],
+	afterUsingBreath: (user, corset) => {},
+};
 
-const corsets = [DEFAULT_CORSET, { name: "Latex Corset", type: "corset_latex" }];
+const corsets = [
+	DEFAULT_CORSET,
+	// is a bit more forgiving on the max breath than the regular corset, but recovery is slower
+	{
+		name: "Latex Corset",
+		type: "corset_latex",
+		maxBreath: [2000, 70, 60, 52, 44, 36, 30, 25, 20, 16, 12, 8, 5, 4, 3, 2],
+		minBreath: [0, -120, -116, -112, -108, -104, -96, -88, -80, -72, -60, -60, -48, -40, -30, -20],
+		breathRecovery: [2000, 4.4, 3.6, 3, 2.4, 1.8, 1.5, 1.2, 0.9, 0.7, 0.5, 0.3, 0.15, 0.08, 0.03, 0.005],
+		gaspCoefficient: 1,
+		gaspLimit: [1000, 28, 24, 20, 17, 14, 12, 10, 8, 6.5, 5, 3.5, 2, 1.5, 1, 1],
+		silenceLimit: [-2000, -56, -48, -40, -34, -28, -24, -20, -16, -13, -10, -7, -4, -3, -2, -2],
+		minWords: [10, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0, 0],
+		afterUsingBreath: (user, corset) => {},
+	},
+	// is much more forgiving in general
+	{
+		name: "Cloth Corset",
+		type: "corset_cloth",
+		maxBreath: [2000, 112, 96, 80, 68, 56, 48, 40, 32, 26, 20, 14, 8, 6, 4, 4],
+		minBreath: [-2000, -11, -96, -80, -68, -56, -48, -40, -32, -26, -20, -14, -8, -6, -4, -4],
+		breathRecovery: [2000, 9.2, 7.6, 6.4, 5.2, 4, 3.2, 2.56, 2, 1.6, 1.2, 0.8, 0.4, 0.2, 0.08, 0.016],
+		gaspCoefficient: 0.8,
+		gaspLimit: [1000, 28, 24, 20, 17, 14, 12, 10, 8, 6.5, 5, 3.5, 2, 1.5, 1, 1],
+		silenceLimit: [-2000, -56, -48, -40, -34, -28, -24, -20, -16, -13, -10, -7, -4, -3, -2, -2],
+		minWords: [10, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0, 0],
+		afterUsingBreath: (user, corset) => {},
+	},
+	// is very punishing
+	{
+		name: "Masterwork Corset",
+		type: "corset_masterwork",
+		maxBreath: [2000, 40, 34, 28, 22.5, 18, 14.5, 12, 10, 8, 6, 4, 2.5, 2, 1.5, 1],
+		minBreath: Array(16).fill(-2000),
+		breathRecovery: [2000, 3.5, 2.9, 2.4, 2, 1.6, 1.25, 0.9, 0.6, 0.4, 0.25, 0.15, 0.08, 0.04, 0.016, 0.004],
+		gaspCoefficient: 2,
+		gaspLimit: [1500, 30, 25.5, 21, 16.875, 13.5, 10.875, 9, 7.5, 6, 4.5, 3, 1.875, 1.5, 1.125, 0.75],
+		silenceLimit: [-2000, -40, -34, -28, -22.5, -18, -14.5, -12, -10, -8, -6, -4, -2.5, -2, -1.5, -1],
+		minWords: Array(16).fill(0),
+		afterUsingBreath: (user, corset) => {},
+	},
+	// tightens after overexertion
+	{
+		name: "Punishment Corset",
+		type: "corset_punishment",
+		maxBreath: [2000, 56, 48, 40, 34, 28, 24, 20, 16, 13, 10, 7, 4, 3, 2, 2],
+		minBreath: [0, -120, -116, -112, -108, -104, -96, -88, -80, -72, -60, -60, -48, -40, -30, -20],
+		breathRecovery: [2000, 4.6, 3.8, 3.2, 2.6, 2, 1.6, 1.28, 1, 0.8, 0.6, 0.4, 0.2, 0.1, 0.04, 0.008],
+		gaspCoefficient: 1.2,
+		gaspLimit: [1000, 28, 24, 20, 17, 14, 12, 10, 8, 6.5, 5, 3.5, 2, 1.5, 1, 1],
+		silenceLimit: [-2000, -56, -48, -40, -34, -28, -24, -20, -16, -13, -10, -7, -4, -3, -2, -2],
+		minWords: [10, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0, 0],
+		afterUsingBreath: (user, corset) => {
+			if (corset.tightness < 11 && corset.breath < corset.minBreath[corset.tightness]) {
+				corset.tightness++;
+			}
+		},
+	},
+];
 
 const lookup = new Map(corsets.map((corset) => [corset.type, corset]));
-
-const MAX_BREATH_TABLE = [2000, 56, 48, 40, 34, 28, 24, 20, 16, 13, 10, 7, 4, 3, 2, 2];
-const MIN_BREATH_TABLE = [0, -120, -116, -112, -108, -104, -96, -88, -80, -72, -60, -60, -48, -40, -30, -20];
-const BREATH_RECOVERY_TABLE = [2000, 4.6, 3.8, 3.2, 2.6, 2, 1.6, 1.28, 1, 0.8, 0.6, 0.4, 0.2, 0.1, 0.04, 0.008];
 
 // NOTE: Encapsulate gaspSounds in EOT characters so the Doll Visor doesn't split on them.
 const gaspSounds = ["*hff*", "*hnnf*", "*ahff*", "*hhh*", "*nnh*", "*hnn*", "*hng*", "*uah*", "*uhf*"];
@@ -26,12 +91,14 @@ const assignCorset = (user, type, tightness, origbinder) => {
 	const old = process.corset[user];
 	const currentBreath = old ? getBreath(user) : null;
 	let originalbinder = old?.origbinder;
+	type ??= old?.type ?? DEFAULT_CORSET.type;
+	const newMaxBreath = lookup.get(type)?.maxBreath ?? DEFAULT_CORSET.maxBreath;
 	process.corset[user] = {
 		tightness: tightness ?? old?.tightness ?? 5,
-		breath: currentBreath ? Math.min(currentBreath, MAX_BREATH_TABLE[tightness]) : MAX_BREATH_TABLE[tightness],
+		breath: currentBreath ? Math.min(currentBreath, newMaxBreath[tightness]) : newMaxBreath[tightness],
 		timestamp: Date.now(),
 		origbinder: originalbinder ?? origbinder, // Preserve original binder until it is removed.
-		type: type ?? old?.type ?? DEFAULT_CORSET.type,
+		type: type,
 	};
 	if (process.readytosave == undefined) {
 		process.readytosave = {};
@@ -128,7 +195,7 @@ function corsetLimitWords(text, parent, user, msgModified) {
 				else if (corset.tightness >= 3 && capitals > 4) syllable = syllable.toLowerCase();
 
 				let ended = false;
-				if (corset.breath < -MAX_BREATH_TABLE[corset.tightness] && i > 5 - Math.ceil(corset.tightness / 2)) {
+				if (corset.breath < corset.silenceLimit[corset.tightness] && i >= corset.minWords[corset.tightness]) {
 					if (!silence) {
 						ended = true;
 						silenceIdx = currIdx;
@@ -138,7 +205,7 @@ function corsetLimitWords(text, parent, user, msgModified) {
 
 				// add gasping sounds once at half of max breath
 				let gasp = "";
-				if (!silence && corset.breath < MAX_BREATH_TABLE[corset.tightness] / 2 && Math.random() < Math.min(corset.tightness / 10, 1 - (Math.max(corset.breath, -MAX_BREATH_TABLE[corset.tightness]) + MAX_BREATH_TABLE[corset.tightness]) / (corset.tightness * MAX_BREATH_TABLE[corset.tightness] * 0.2))) {
+				if (!silence && corset.breath < corset.gaspLimit[corset.tightness] && Math.random() < corset.gaspCoefficient * Math.min(corset.tightness / 10, 1 - (corset.breath - corset.silenceLimit[corset.tightness]) / (corset.gaspLimit[corset.tightness] - corset.silenceLimit[corset.tightness]))) {
 					if (j == 0) gasp = gaspSounds[Math.floor(Math.random() * gaspSounds.length)] + " ";
 					else gasp = "-" + gaspSounds[Math.floor(Math.random() * gaspSounds.length)] + "-";
 				}
@@ -153,6 +220,8 @@ function corsetLimitWords(text, parent, user, msgModified) {
 			currIdx++;
 		}
 	}
+
+	corset.afterUsingBreath(user, corset);
 
 	let outtext = (silence ? chars.slice(0, silenceIdx + 1) : chars).join("");
 
@@ -174,10 +243,9 @@ function corsetLimitWords(text, parent, user, msgModified) {
 
 // calculates current breath and returns corset. Does not save to file.
 function calcBreath(user) {
-	if (process.corset == undefined) process.corset = {};
-	const corset = process.corset[user];
+	const corset = getCorset(user);
 	if (!corset) return null;
-	if (corset.breath < MIN_BREATH_TABLE[corset.tightness]) corset.breath = MIN_BREATH_TABLE[corset.tightness];
+	if (corset.breath < corset.minBreath[corset.tightness]) corset.breath = corset.minBreath[corset.tightness];
 	const now = Date.now();
 	let recoveryCoefficient = 1;
 	if (process.gags == undefined) process.gags = {};
@@ -192,8 +260,8 @@ function calcBreath(user) {
 			}
 		});
 	}
-	const newBreath = corset.breath + BREATH_RECOVERY_TABLE[corset.tightness] * ((now - corset.timestamp) / 1000) * recoveryCoefficient;
-	if (newBreath > MAX_BREATH_TABLE[corset.tightness]) corset.breath = MAX_BREATH_TABLE[corset.tightness];
+	const newBreath = corset.breath + corset.breathRecovery[corset.tightness] * ((now - corset.timestamp) / 1000) * recoveryCoefficient;
+	if (newBreath > corset.maxBreath[corset.tightness]) corset.breath = corset.maxBreath[corset.tightness];
 	else corset.breath = newBreath;
 	corset.timestamp = now;
 	return corset;
@@ -212,6 +280,7 @@ function getBreath(user) {
 function tryExpendBreath(user, exertion) {
 	const corset = calcBreath(user);
 	corset.breath -= exertion;
+	corset.afterUsingBreath(user, corset);
 	if (process.readytosave == undefined) {
 		process.readytosave = {};
 	}
