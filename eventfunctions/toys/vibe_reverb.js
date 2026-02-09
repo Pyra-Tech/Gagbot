8f263310ea6293f12c8ce982a6064cdd2d4ce15a
Pyrotech <pyratechnician@gmail.com>
2026-02-09 03:41:39 +0000

    Initial Regex checks

--- eventfunctions/toys/vibe_reverb.js
+++ eventfunctions/toys/vibe_reverb.js
@@ -7,7 +7,10 @@ const decay_period = 30000
 
 function msgfunction(userid, data) {
 
-    // Catch Message, Update End Time and Increment Vibe Intensity    
+    // Catch Message, and Check for OOC, Whispers, or Shouting
+    intensity = volumetest(data.msgcontent)
+
+    //Update End Time and Increment Vibe Intensity    
     if(getUserVar(userid, "reverbEndTime") == undefined) {
         // Set initial 3 minute timer
         setUserVar(userid, "reverbEndTime", Date.now() + initial_timespan);
@@ -40,6 +43,55 @@ async function functiontick(userID) {
         setUserVar(userID, "reverbVibeIntensity", Math.max(getUserVar(userID, "reverbVibeIntensity") - 1, 0));
         setUserVar(userID, "reverbDecayTime", Date.now() + decay_period);
     }
+}
+
+function volumetest(message) {
+    // Split into lines
+
+    // Discard OOC Lines
+    let OOC = new RegExp(/^[*][^*].*[^*][*]$/, "g");
+    
+        if(message.match(OOC))
+        {
+            console.log(`OOC: ${message}`);
+            return 0
+        }
+
+        // Check for Shout in remaining message chunks
+        //let loud = new RegExp("**", "g");
+        if(message.match(/(\b[A-Z]['A-Z]+|\b[A-Z]\b)/g))
+        {
+            console.log("SHOUT DETECTED");
+            return 4
+        } 
+
+    // Check for Loudness across all lines. Update Intensity if greater, and default to 0 in the case of no text
+    let intensity = 0;
+    
+    /*/
+
+    msgLines.forEach(line => {
+        switch(line) {
+            //**ALLCAPS**
+            case(line.match(/(\b[A-Z]['A-Z]+|\b[A-Z]\b)/g)):                     
+                return 4;
+            //**Message** OR ALLCAPS
+            case((line.match(loud) || []).length > 2):                            
+                if(intensity < 2)
+                    intensity = 2;
+            //-# Message
+            case(line.startsWith("-#")):                     
+                if(intensity < 0.5)
+                    intensity = 0.5;
+            //Regular Text
+            default:                                        
+                if(intensity < 1)
+                    intensity = 1;
+        }
+    });
+    //*/
+
+    return intensity;
 }
 
 exports.functiontick = functiontick;
