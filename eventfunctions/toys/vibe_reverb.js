98ad3d4fc653a8c78a52204ae74f5b773da00299
Pyrotech <pyratechnician@gmail.com>
2026-02-10 01:54:16 +0000

    Finalised message filtering and checks on the contents of each line. Now alters the scaling based on how loud the person is being, both in how slowly it begins to decay again, and in how much the vibrations ramp up.

--- eventfunctions/toys/vibe_reverb.js
+++ eventfunctions/toys/vibe_reverb.js
@@ -5,93 +5,110 @@ const initial_timespan = 180000
 const timespan_inc = 15000
 const decay_period = 30000
 
+const newline = new RegExp(/[^\r\n]+/g)
+const OOC = new RegExp(/^[*][^*].*[^*][*]$/)
+const LOUD = new RegExp(/(\b[A-Z]['A-Z]+|\b[A-Z]\b)/)
+const BOLD = new RegExp(/([\*][\*])/)
+
 function msgfunction(userid, data) {
 
     // Catch Message, and Check for OOC, Whispers, or Shouting
     intensity = volumetest(data.msgcontent)
+    //console.log(intensity);
 
     //Update End Time and Increment Vibe Intensity    
     if(getUserVar(userid, "reverbEndTime") == undefined) {
         // Set initial 3 minute timer
         setUserVar(userid, "reverbEndTime", Date.now() + initial_timespan);
     } else {
-        // Increment by 15s per Message
-        setUserVar(userid, "reverbEndTime", getUserVar(userid, "reverbEndTime") + timespan_inc);
+        // Increment reverbEndTime by 15s x intensity per Message 
+        setUserVar(userid, "reverbEndTime", getUserVar(userid, "reverbEndTime") + (timespan_inc * intensity));
     }
 
-    // Declare Initial Decay Time
+    // Declare Initial reverbDecayTime
     if(getUserVar(userid, "reverbDecayTime") == undefined) {
-        setUserVar(userid, "reverbDecayTime", Date.now() + decay_period);
+        setUserVar(userid, "reverbDecayTime", Date.now() + (decay_period * intensity));
+    }
+    else
+    {
+        // Override Next Decay time with a new value based on the intensity if it is longer than the current delay
+        setUserVar(userid, "reverbDecayTime", Math.max(Date.now() + (decay_period * intensity), getUserVar(userid, "reverbDecayTime")));
     }
 
-    // Increment Intensity Modifier
-    setUserVar(userid, "reverbVibeIntensity", Math.min(getUserVar(userid, "reverbVibeIntensity") + 1, 20));
+    // Increment reverbVibeIntensity based on intensity of message text
+    setUserVar(userid, "reverbVibeIntensity", Math.min(getUserVar(userid, "reverbVibeIntensity") + (1 * intensity), 20));
     return;
 }
 
 async function functiontick(userID) {
-    // Clear Values When Vibe Stops
-    if (getUserVar(userID, "reverbEndTime") < Date.now() && getUserVar(userID, "reverbVibeIntensity") == 0) {
-        console.log(`${userID}'s Reverb Vibe has stopped`)
-        setUserVar(userID, "reverbEndTime", undefined)
-        setUserVar(userID, "reverbDecayTime", undefined)
-    }
-
     // Decay Intensity every Decay Period until 0
     if (getUserVar(userID, "reverbDecayTime") < Date.now() && getUserVar(userID, "reverbDecayTime") != undefined)
     {
         setUserVar(userID, "reverbVibeIntensity", Math.max(getUserVar(userID, "reverbVibeIntensity") - 1, 0));
         setUserVar(userID, "reverbDecayTime", Date.now() + decay_period);
     }
+
+    // Clear Values When Vibe Stops
+    if (getUserVar(userID, "reverbEndTime") < Date.now() && getUserVar(userID, "reverbVibeIntensity") == 0) {
+        console.log(`${userID}'s Reverb Vibe has stopped`)
+        setUserVar(userID, "reverbEndTime", undefined)
+        setUserVar(userID, "reverbDecayTime", undefined)
+    }
 }
 
 function volumetest(message) {
     // Split into lines
+    arrayOfLines = message.match(newline);
+    console.log(`ARRAY OF LINES: ${arrayOfLines}\n`);
 
+    intensityMod = 0;
     // Discard OOC Lines
-    let OOC = new RegExp(/^[*][^*].*[^*][*]$/, "g");
-    
-        if(message.match(OOC))
-        {
-            console.log(`OOC: ${message}`);
-            return 0
+    arrayOfLines.forEach((line) => {
+
+        // Check for Whispers
+        let Whisper = false;    
+        if(line.startsWith("-#"))
+        {            
+            Whisper = true;
+            line = line.replace("-# ", "");
         }
 
-        // Check for Shout in remaining message chunks
-        //let loud = new RegExp("**", "g");
-        if(message.match(/(\b[A-Z]['A-Z]+|\b[A-Z]\b)/g))
+        // Ignore OOC Lines
+        if(OOC.test(line))
         {
-            console.log("SHOUT DETECTED");
-            return 4
-        } 
-
-    // Check for Loudness across all lines. Update Intensity if greater, and default to 0 in the case of no text
-    let intensity = 0;
-    
-    /*/
-
-    msgLines.forEach(line => {
-        switch(line) {
-            //**ALLCAPS**
-            case(line.match(/(\b[A-Z]['A-Z]+|\b[A-Z]\b)/g)):                     
-                return 4;
-            //**Message** OR ALLCAPS
-            case((line.match(loud) || []).length > 2):                            
-                if(intensity < 2)
-                    intensity = 2;
-            //-# Message
-            case(line.startsWith("-#")):                     
-                if(intensity < 0.5)
-                    intensity = 0.5;
-            //Regular Text
-            default:                                        
-                if(intensity < 1)
-                    intensity = 1;
+            //console.log(`OOC: ${line} : ${Whisper}`);
+        }
+        else if(!Whisper && LOUD.test(line) && (line.match(BOLD) || []).length >= 2)
+        {
+            //console.log(`BRIAN: ${line} : ${Whisper}`);
+            return intensityMod = 4;
+        }
+        else if(!Whisper && LOUD.test(line)){
+            if(intensityMod < 3) intensityMod = 3;
+            //console.log(`LOUD: ${line} : ${Whisper}`);
+        }                
+        else if(!Whisper && (line.match(BOLD) || []).length >= 2)
+        {               
+            if(intensityMod < 2) intensityMod = 2;
+            //console.log(`BOLD : ${line} : ${Whisper}`);
+        }
+        else if(Whisper && (line.match(BOLD) || []).length >= 2)
+        {
+            if(intensityMod < 0.75) intensityMod = 0.75;
+            //console.log(`BOLD Whisper: ${line} : ${Whisper}`);
+        }
+        else if(Whisper)
+        {
+            if(intensityMod < 0.5) intensityMod = 0.5;
+            //console.log(`Whisper: ${line} : ${Whisper}`);
+        }
+        else
+        {
+            if(intensityMod < 1) intensityMod = 1;
+            //console.log(`Normal: ${line} : ${Whisper}`);
         }
     });
-    //*/
-
-    return intensity;
+    return intensityMod;
 }
 
 exports.functiontick = functiontick;
