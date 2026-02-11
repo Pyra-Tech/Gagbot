cf765c74e69a881dfe90a6b931679586894f4dcc
Enraa <enraaaery@gmail.com>
2026-02-10 20:46:02 -0800

    lol

--- commands/mask.js
+++ commands/mask.js
@@ -170,7 +170,6 @@ module.exports = {
 						} else {
 							// Not wearing it!
 							data.noworn = true;
-                            console.log(process.modalfunctions?.headwear)
 							if (process.modalfunctions?.headwear && process.modalfunctions.headwear[headwearchoice]) {
                                 await interaction.showModal(await process.modalfunctions.headwear[headwearchoice](interaction, headwearuser.id))
                                 interaction.followUp(getText(data));
@@ -194,9 +193,9 @@ module.exports = {
 								} else {
 									// Not wearing it!
 									data.noworn = true;
-                                    if (process.eventfunctions?.headwear && process.eventfunctions.headwear[headwearchoice] && process.eventfunctions.headwear[headwearchoice].modal) {
-                                        await interaction.showModal(process.eventfunctions.headwear[headwearchoice].modal(interaction, headwearuser.id))
-                                        interaction.update(getText(data));
+                                    if (process.modalfunctions?.headwear && process.modalfunctions.headwear[headwearchoice]) {
+                                        await interaction.showModal(await process.modalfunctions.headwear[headwearchoice](interaction, headwearuser.id))
+                                        interaction.followUp(getText(data));
                                     }
                                     else {
                                         interaction.reply(getText(data));
