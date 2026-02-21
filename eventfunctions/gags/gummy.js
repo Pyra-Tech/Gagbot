28d0b0a3878234fe75202b82ab6b7864a1894bef
Pyrotech <pyratechnician@gmail.com>
2026-02-21 23:24:51 +0000

    Added Gummy Gag

---
+++ eventfunctions/gags/gummy.js
@@ -0,0 +1,36 @@
+const { getOption } = require("../../functions/configfunctions")
+const { messageSendChannel } = require("../../functions/messagefunctions")
+const { getUserVar, setUserVar } = require("../../functions/usercontext")
+const { getGag, assignGag, deleteGag} = require("../../functions/gagfunctions.js");
+
+const DISSOLVE_RATE_MS = 300000;
+
+async function functiontick(userID) {
+    // Init Countdown Variable on First Run if not already present
+    if (getUserVar(userID, "confectionaryDissolveTimer") == undefined) {
+        setUserVar(userID, "confectionaryDissolveTimer", Date.now() + DISSOLVE_RATE_MS)
+        console.log(getGag(userID, "gummy"), ": ", getGag(userID, "gummy").intensity)
+        messageSendChannel(`<@${userID}> starts to suck on their gummy gag... this won't take long~`, process.recentmessages[userID])
+    }
+
+    // Decrement Intensity every timer interval
+    if (getUserVar(userID, "confectionaryDissolveTimer") < Date.now() && getGag(userID, "gummy")) {
+        console.log(getGag(userID, "gummy"), ": ",getGag(userID), ": ", getGag(userID, "gummy").intensity)
+        if(getGag(userID, "gummy").intensity > 1){
+            setUserVar(userID, "confectionaryDissolveTimer", Date.now() + DISSOLVE_RATE_MS)
+            // Get Intensity and push decremented version
+            let oldIntensity = getGag(userID, "gummy").intensity
+            assignGag(userID, "gummy", oldIntensity - 1)
+
+            messageSendChannel(`<@${userID}>'s gummy gag has shrunk~`, process.recentmessages[userID])
+        }
+        else {
+            // Clear Gag and Dissolve Timer
+            setUserVar(userID, "confectionaryDissolveTimer", undefined)
+            deleteGag(userID, "gummy")
+            messageSendChannel(`<@${userID}>'s gummy gag has dissolved away~`, process.recentmessages[userID])
+        }
+    }
+}
+
+exports.functiontick = functiontick;
